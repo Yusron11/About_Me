06b95bc2e8ef9d7ebc2e0e948b1d4f7e466584be
Ahmad Yusron <94107457+Yusron11@users.noreply.github.com>
2021-11-17 01:38:17 +0700

    Add files via upload

---
+++ AboutMe.py
@@ -0,0 +1,10 @@
+import os
+os.system ('cls')
+
+Nama = 'Ahmad Yusron'
+Umur = '18 Tahun'
+Alamat = 'Jember - Jawa Timur'
+Pendidikan = 'Teknologi Informasi - Universitas Jember'
+
+print (f'' "Nama :" , Nama, "\n" "Umur :", Umur, "\n" "Alamat :", Alamat, "\n" "Pendidikan :", Pendidikan)
+print ('=' * 10, 'TERIMAKASIH', '=' * 10, "\n")
